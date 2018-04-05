570d0344ec12e4982c8a2347037e1a6dbb972b06
ThaBullfrog <th3bullfrog@gmail.com>
2018-04-05 17:18:34 -0400

    apply small fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.relative_url_root = '/redesign'
+  config.action_controller.relative_url_root = '/redesign'
 end
