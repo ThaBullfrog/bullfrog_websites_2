6b1cf1bf71399ea624ea3497f9981c4a4a456c33
ThaBullfrog <th3bullfrog@gmail.com>
2018-04-08 15:39:56 -0400

    complete work page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def title
+    base_title = "Bullfrog Websites"
+    @title ? "#{@title} | #{base_title}" : base_title
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/work', to: 'static_pages#work'
+  get '/contact', to: 'static_pages#contact'
 end
